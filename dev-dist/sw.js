4a50dec2cfb35cde95e4792ddcccda02e04eeb82
Rishab Sriram <rishabsr25@gmail.com>
2026-05-17 15:59:34 -0400

    feat: rename mockData to parkData and add photo upload to Park Watch

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.8s2ss1b3c9g"
+    "revision": "0.i1uv77t010o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
